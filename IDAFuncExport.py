637ccb8c43142522b528a228fc73dc546e799b4b
Jianwei <hjw@tamu.edu>
2023-09-01 14:22:02 -0500

    update IDAFuncExport to Python3 and IDAPython 7.4+

--- IDAFuncExport.py
+++ IDAFuncExport.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-from idc import *
-from idautils import *
-from idaapi import *
-
+import ida_auto, idc, ida_ida
 import re
+import idautils, idaapi, ida_funcs, ida_bytes
 
-import sys, os
-reload(sys)
-sys.setdefaultencoding("utf-8")
+import os
 
-script_path = "/Volumes/Work/loccs/code/Corgi/"
+script_path = "Z:\projects\Corgi"
 class_filtered_dir = "class_filter_result"
 
 # 匹配pattern
@@ -23,38 +16,44 @@ pattern_list = [
 def GetFunctionList():
 	candidate = []
 
-	Wait()
+	ida_auto.auto_wait()
 
-	ea = BeginEA()
+	ea = ida_ida.inf_get_min_ea()
 	# ea = 0x0000000000001E80
 
 	pattern = []
 	for i in pattern_list:
 		pattern.append(re.compile(i))
 
-	for funcea in Functions(SegStart(ea), SegEnd(ea)):
-		fn = GetFunctionName(funcea)
-		match_flag = False
-		for i in pattern:
-			if i.match(fn) != None:
-				match_flag = True
-				break
+	for s in idautils.Segments():
+		start = idc.get_segm_start(s)
+		end = idc.get_segm_end(s)
+		name = idc.get_segm_name(s)
+		data = ida_bytes.get_bytes(start, end-start)
 
-		if not match_flag and fn.find('[') != -1 and fn.find(']') != -1:
-			candidate.append(fn)
+		for funcea in idautils.Functions(start, end):
+			fn = ida_funcs.get_func_name(funcea)
+			match_flag = False
+			for i in pattern:
+				if i.match(fn) != None:
+					match_flag = True
+					break
+
+			if not match_flag and fn.find('[') != -1 and fn.find(']') != -1:
+				candidate.append(fn)
 
 	return candidate
 
 def WriteListToFile(filepath, data):
 	with open(filepath, 'w+') as f:
 		for i in data:
-			print >> f, i
+			print(i, file=f)
 
 
 def main():
 	FuncList = GetFunctionList()
 	path = os.path.join(script_path, class_filtered_dir)
-	filename = get_root_filename()
+	filename = idaapi.get_root_filename()
 	WriteListToFile(os.path.join(path, filename + "_origin_class.txt"), FuncList)
 
 if __name__ == '__main__':
